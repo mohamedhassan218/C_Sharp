f3c8d4b0874270ff0eb580cd1523e4efcf8f5678
mohamedhassan218 <m.hassan.def@gmail.com>
2022-08-22 21:09:54 +0200

    Add the rest of lesson 4

--- Lesson 4 .cs
+++ Lesson 4 .cs
@@ -33,6 +33,34 @@ public class HelloWorld
         string end = "Everyone lived happily ever after.";
         string story = $"{beginning}{middle}{end}"; 
         Console.WriteLine(story);
+
+
+        //Properities of string variables:
+       //{ Length, IndexOf(), Substring(), ToUpper(), ToLower() }
+       string name = Console.ReadLine();
+       int len = name.Length;
+       Console.WriteLine(len);
+       
+       int indxOfM = name.IndexOf("M");
+       Console.WriteLine($"And the index of the char M is { indxOfM }");
+       
+       string e = "I'm learning C# on online compiler";
+       int tempINDX = e.IndexOf("online");//returns the index of the 'o'
+       Console.WriteLine(tempINDX);   //Prints: 19
+       string mySubString = e.Substring(19);
+       Console.WriteLine(mySubString); //Prints: online compiler
+       //It takes from the 19 index to the end of the string.
+       int lengthOfSubString = 6;
+       mySubString = e.Substring(19, lengthOfSubString);
+       Console.WriteLine(mySubString);  //Prints: online
+       //Here, it takes from the beginning index (19) 
+       //a substring of length of 6 chars.
+       
+       string shouting = "I'm not shouting, U are shouting!".ToUpper();
+       Console.WriteLine(shouting); //Prints: I'M NOT SHOUTING, U ARE SHOUTING!
+       shouting = shouting.ToLower();
+       Console.WriteLine(shouting); //Prints:i'm not shouting, u are shouting!
+       
         
         
     }
