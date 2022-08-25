ccf7e50d5956050425253d2914494645487b63a3
mohamedhassan218 <m.hassan.def@gmail.com>
2022-08-25 19:15:32 +0200

    how to put value for specific varible of the parameters, methods overloading

--- Lesson 7 .cs
+++ Lesson 7 .cs
@@ -16,6 +16,21 @@ public class HelloWorld
         
         //Call your own method:
         methodName();
+
+
+        method1(8);//8
+        method1();//0
+        
+        //to put value to a specific variable and not the all
+        //First, sure that U have put default value for each variable
+        method2(c : 89);
+        //if U enter them mentioning each parameter name, U can put them in any order.
+        method2(c : 9, b : 110, a : 123);
+        
+        //Calling methods with same name but different parameters:
+        NamePets("KOKO", "MAEA");
+        NamePets("KOKO", "MAEA", "LOLO");
+        NamePets();
         
         
     }
@@ -25,4 +40,34 @@ public class HelloWorld
     {
         Console.WriteLine("Hi, I'm your method!");
     }
+
+
+    //the method must be static to call it without taking a reference from the class.
+    //0 here is a default value , if the user call the method without put any number.
+    static void method1(int number = 0)
+    {
+        Console.WriteLine($"The number is {number}");
+    }
+    
+    static void method2(int a = 0, int b = 0, int c = 0)
+    {
+        Console.WriteLine($"a is {a}\nb is {b}\nc is {c}");
+    }
+    
+    //Lets practice with method overloads:
+     static void NamePets(string s1, string s2)
+    {
+      Console.WriteLine($"Your pets {s1} and {s2} will be joining your voyage across space!");
+    }
+    
+    static void NamePets(string s1, string s2, string s3)
+    {
+      Console.WriteLine($"Your pets {s1}, {s2}, and {s3} will be joining your voyage across space!");
+    }
+
+    static void NamePets()
+    {
+      Console.WriteLine("Aw, you have no spacefaring pets :(");
+    }
+    
 }
